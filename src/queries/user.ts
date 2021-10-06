0b72528b5815880b9e0b2a522d0f8e648cea4706
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-10-06 14:31:18 +0200

    list users

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -1,13 +1,14 @@
 import { argsToArgsConfig } from "graphql/type/definition";
-import { arg, extendType, list, mutationField, queryType, stringArg } from "nexus";
+import { arg, extendType, list, mutationField, nonNull, queryField, queryType, stringArg } from "nexus";
 import { type } from "os";
 import { User } from "../models";
 
-const QueryUsers = queryType({
-    definition(t) {
-        t.field('users', {
-            type: list(User)
-        })
+const QueryUsers = queryField('users', {
+    type: list(User),
+    args: {
+    },
+    resolve(_root, _args, ctx){
+        return ctx.db.user.findMany()
     }
 })
 
